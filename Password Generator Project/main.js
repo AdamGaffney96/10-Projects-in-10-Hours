f58325fc1106456b2b70eeaf0e603a29c59d466d
Adam Gaffney <adamgaffney96@gmail.com>
2022-08-01 23:17:18 +0100

    Changed password gen to use localStorage

--- Password Generator Project/main.js
+++ Password Generator Project/main.js
@@ -8,8 +8,8 @@ const passwordLengthSlider = document.querySelector(".password-length-slider");
 const passwordText = document.querySelector(".password-text");
 
 // Calculated variables
-const barRightBound = Math.round(passwordLengthBar.getBoundingClientRect().right) + 114;
-const barLeftBound = Math.round(passwordLengthBar.getBoundingClientRect().right) - 114;
+const barRightBound = Math.round(passwordLengthBar.getBoundingClientRect().right) + 228;
+const barLeftBound = Math.round(passwordLengthBar.getBoundingClientRect().right);
 
 // Fixed variables
 const minLength = 6;
@@ -26,15 +26,20 @@ const changeBarWidthOnMove = event => {
     document.onselectstart = event.preventDefault();
     // Calculates bar width
     if (event.clientX < barRightBound && event.clientX > barLeftBound) {
-        passwordLengthBar.style.width = `${passwordLengthBar.offsetWidth + event.movementX}px`;
+        setBarWidth(event.movementX);
     }
     // Set password number
     let newLength = calculatePasswordLength(passwordLengthBar.offsetWidth, minLength, maxLength);
     if (newLength != passwordLengthNumber.innerHTML) {
         passwordLengthNumber.innerHTML = newLength;
         setPassword();
+        saveToLocal();
     }
 };
+
+const setBarWidth = change => {
+    passwordLengthBar.style.width = `${passwordLengthBar.offsetWidth + change}px`;
+}
 
 const calculatePasswordLength = (barWidth, minLength, maxLength) => {
     let diff = maxLength - minLength;
@@ -66,7 +71,7 @@ const setPassword = () => {
     passwordText.innerHTML = calculatePassword();
 }
 
-copyPassword = () => {
+const copyPassword = () => {
     navigator.clipboard.writeText(passwordText.innerText);
     document.querySelector(".copied-alert").style.display = "inherit";
     setTimeout(e => {
@@ -79,13 +84,37 @@ copyPassword = () => {
     console.log("Password copied!");
 }
 
+const saveToLocal = () => {
+    const settings = {
+        "length": document.querySelector(".password-length-num").innerHTML,
+        "uppercase": document.getElementById("uppercase").checked,
+        "digits": document.getElementById("numerics").checked,
+        "symbols": document.getElementById("symbols").checked
+    }
+    localStorage.setItem("settings", JSON.stringify(settings));
+    console.log("%cSaved to local storage.", 'color: red; font-size: 20px;');
+}
+
+const loadFromLocal = () => {
+    const settings = JSON.parse(localStorage.getItem("settings"));
+    document.querySelector(".password-length-num").innerHTML = settings["length"];
+    document.querySelector("#uppercase").checked = settings["uppercase"];
+    document.querySelector("#numerics").checked = settings["digits"];
+    document.querySelector("#symbols").checked = settings["symbols"];
+    console.log("%cLoaded from local storage.", 'color: red; font-size: 20px;');
+}
+
 const init = () => {
-    passwordLengthNumber.innerHTML = calculatePasswordLength(passwordLengthBar.offsetWidth, minLength, maxLength);
-    setPassword();
+    if (localStorage.getItem("settings") == null) {
+        passwordLengthNumber.innerHTML = calculatePasswordLength(passwordLengthBar.offsetWidth, minLength, maxLength);
+    }
     document.querySelectorAll("input[type='checkbox']").forEach(checkbox => {
         checkbox.addEventListener("change", setPassword);
     })
     document.querySelector(".password-head").addEventListener("click", copyPassword);
+    loadFromLocal();
+    setBarWidth(Math.ceil(200 / (maxLength - minLength)) * (parseInt(passwordLengthNumber.innerHTML) - 6));
+    setPassword();
 }
 
 passwordLengthSlider.addEventListener("mousedown", e => {
@@ -95,6 +124,10 @@ passwordLengthSlider.addEventListener("mousedown", e => {
 window.addEventListener("mouseup", e => {
     window.removeEventListener("mousemove", changeBarWidthOnMove);
 });
+
+document.querySelectorAll("[type='checkbox']").forEach(box => {
+    box.addEventListener("change", saveToLocal);
+})
 
 // Run initial functions
 init();
